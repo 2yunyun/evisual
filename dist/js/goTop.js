68bb30989088e8f188a5a941198b14b18bfeffca
zhangyun <18701386706@163.com>
2020-05-15 14:21:27 +0800

    更新

--- dist/js/goTop.js
+++ dist/js/goTop.js
@@ -11,4 +11,24 @@ $(function() {
             scrollTop: '0px'
         }, 800);
     });
+
+
+// 模块出现时机
+$('.j-consoltx').css({
+    top:$(window).height()/2
+});
+if($.browser.msie && $.browser.version <9) $('.j-consoltx').addClass('z-show');
+$(window).scroll(function (event) {
+    if($.browser.msie && $.browser.version <9) return;
+    var screenHeight = $(window).height();
+    var bannerHeight = $('.j-swiper').height();
+    if(bannerHeight < $('.j-consoltx').offset().top){
+        $('.j-consoltx').addClass('z-show')
+    }else{
+        $('.j-consoltx').removeClass('z-show')
+    }
+});
+
+
+
 });
